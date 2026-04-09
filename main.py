a531f5b112f3955c242f3b911cc9397fb64875cc
Dudha, Mufeed <eog24001@uconn.edu>
2026-04-09 12:40:26 -0400

    New Files for MileStone3

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ uni = University()
 load_courses("course_catalog_CSE10_with_capacity.csv", uni)
 load_students("enrollments_CSE10.csv", uni)
 
-from demo_milestone2 import run_milestone2_demo
+from demo_milestone3 import run_milestone2_demo
 
 # Uncomment the lines below to run the demo
 #if __name__ == "__main__":
